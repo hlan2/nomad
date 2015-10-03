1200a7470c0d3d8e0b58e41f4c0056fb299c56ef
Henry Lan <henrylan@Henrys-MacBook-Air.local>
2015-10-03 15:48:04 -0400

    Fixed some association errors and populated database with sample data for each table

--- nomad/app/models/user.rb
+++ nomad/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :reviews
-	has_many :sent_messages, class_name :Message, foreign_key :sender_id
-	has_many :received_messages, class_name :Message, foreign_key :recipient_id
+	has_many :sent_messages, class_name: Message
+	has_many :received_messages, class_name: Message
 end
