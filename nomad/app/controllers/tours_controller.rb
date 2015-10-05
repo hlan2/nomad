b2cedaf97bb75bb9aa095e516ba790a2b2cf8661
Henry Lan <henrylan@Henrys-MacBook-Air.local>
2015-10-05 19:32:27 -0400

    Add tour functionality added

--- nomad/app/controllers/tours_controller.rb
+++ nomad/app/controllers/tours_controller.rb
@@ -7,4 +7,21 @@ class ToursController < ApplicationController
   	@tour = Tour.find(params[:id])
   end
   
-end
+  def new
+  	@tour = Tour.new
+  end
+
+  def create
+  	@tour = Tour.new(tour_params)
+  	if @tour.save
+  		redirect_to tours_path
+  	else
+  		render :new
+  	end
+  end
+
+  private
+  def tour_params
+  	params.require(:tour).permit(:tour_name, :tour_description)
+  end
+ end
