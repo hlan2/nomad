03d1689b6bca1b6a3e3045f795f5db6881e4ce7a
Rachel Cravit <rachel.cravit@gmail.com>
2015-10-14 12:21:58 -0400

    updated database

--- nomad/db/migrate/20151014155058_add_tour_options_to_tours.rb
+++ nomad/db/migrate/20151014155058_add_tour_options_to_tours.rb
@@ -1,4 +1,7 @@
 class AddTourOptionsToTours < ActiveRecord::Migration
   def change
+    add_column :tours, :dog_friendly, :boolean
+    add_column :tours, :family_friendly, :boolean
+    add_column :tours, :adults_only, :boolean
   end
 end
