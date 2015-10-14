e6fca55a43383466781bb1eac1c3aef8a2c68f56
Rachel Cravit <rachel.cravit@gmail.com>
2015-10-14 12:30:13 -0400

    form checkboxes working

--- nomad/db/migrate/20151014155058_add_tour_options_to_tours.rb
+++ nomad/db/migrate/20151014155058_add_tour_options_to_tours.rb
@@ -2,6 +2,5 @@ class AddTourOptionsToTours < ActiveRecord::Migration
   def change
     add_column :tours, :dog_friendly, :boolean
     add_column :tours, :family_friendly, :boolean
-    add_column :tours, :adults_only, :boolean
   end
 end
--- nomad/db/schema.rb
+++ nomad/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151010195507) do
+ActiveRecord::Schema.define(version: 20151014155058) do
 
   create_table "messages", force: :cascade do |t|
     t.integer  "sender_id"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20151010195507) do
     t.float    "longitude"
     t.string   "address"
     t.string   "city"
+    t.boolean  "dog_friendly"
+    t.boolean  "family_friendly"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,9 +52,6 @@ ActiveRecord::Schema.define(version: 20151010195507) do
     t.string   "password_digest"
     t.string   "firstname"
     t.string   "lastname"
-    t.boolean "dog_friendly"
-    t.boolean "family_friendly"
-    t.boolean "adults_only"
   end
 
 end
